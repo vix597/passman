9ca4e6876f4aed2858a849741c46286cc54dbaa2
Sean LaPlante <laplante.sean@gmail.com>
2015-11-10 18:14:12 -0500

    Using PBKDF2 for AES key generation

--- crypt.py
+++ crypt.py
@@ -1,26 +1,30 @@
 import struct
 from Crypto import Random
 from Crypto.Cipher import AES
-from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import PBKDF2
 
 class FileCryptoTool:
     IV_SIZE = 16
+    ITERATIONS = 100000
+    SALT_SIZE = 32
+    KEY_SIZE = 32
     
     def __init__(self):
         pass
         
     def encrypt_file(password, path, file_data):
-        key = SHA256.new(password.encode('utf-8')).digest()
+        salt = Random.new().read(FileCryptoTool.SALT_SIZE)
         iv = Random.new().read(FileCryptoTool.IV_SIZE)
+        key = PBKDF2(password, salt, dkLen=FileCryptoTool.KEY_SIZE, count=FileCryptoTool.ITERATIONS)
         encryptor = AES.new(key, AES.MODE_CFB, iv)
         
         with open(path,'wb') as out_file:
-            out_file.write(iv + encryptor.encrypt(file_data))
+            out_file.write(salt + iv + encryptor.encrypt(file_data))
         
     def decrypt_file(password, path):
-        key = SHA256.new(password.encode('utf-8')).digest()
-        
         with open(path,'rb') as in_file:
+            salt = in_file.read(FileCryptoTool.SALT_SIZE)
             iv = in_file.read(FileCryptoTool.IV_SIZE)
+            key = PBKDF2(password, salt, dkLen=FileCryptoTool.KEY_SIZE, count=FileCryptoTool.ITERATIONS)
             decryptor = AES.new(key, AES.MODE_CFB, iv)
             return decryptor.decrypt(in_file.read())
