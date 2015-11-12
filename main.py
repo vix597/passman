8053c53a3e99b4d7f06b146335e749c30ab4495a
Sean LaPlante <laplante.sean@gmail.com>
2015-11-11 22:21:33 -0500

    Renamed passman->passmancli Continued learning kivy

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-'''
-Use Ubuntu Kivy VM with buildozer to build APK
-'''
 import sys
 import os
 import kivy
@@ -10,13 +7,25 @@ __version__ = "0.0.1"
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.listview import ListView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 
 from passgen import PassGen
 from passdb import PassDb, AccountExistsError, AccountDoesNotExistError, DbLoadError, DbSaveError
 
-class TestApp(App):
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Passphrase"))
+        self.passphrase = TextInput(password=True,multiline=False)
+        self.add_widget(self.passphrase)
+
+class PassManApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return LoginScreen()
 
 if __name__ == '__main__':
-    TestApp().run()
+    PassManApp().run()
